6de5202cda2f08d2b457af369a1c5a7786676e9d
susanwalker <susanwalker8@gmail.com>
2020-06-06 13:54:03 -0400

    Add treats to contoller and views

--- game.rb
+++ game.rb
@@ -4,6 +4,7 @@
 # Allows game.rb to use Cat defined in models/cat.rb
 require './models/cat'
 require './models/maze'
+require './models/treat'
 require './controllers/action_controller'
 require './views/terminal'
 
@@ -13,22 +14,29 @@ cat = Cat.new(0, 0)
 # backtics are used call terminal commands
 # backtics always return a string because that's terminal output, so
 # we call to_i to convert the string to integer
+# termimal width and height need to be integers bc we are comparing them to
+# cat position
 terminal_height = `tput li`.to_i
 terminal_width = `tput co`.to_i
 
 maze = Maze.new(terminal_height, terminal_width)
 
-terminal = Terminal.new(cat)
+treats = [
+  Treat.new(terminal_width / 2, terminal_height / 2),
+  Treat.new(terminal_width / 2, 0)
+]
+
+terminal = Terminal.new(cat, treats)
 terminal.init
-terminal.render_cat
+terminal.render
 
 loop do
   input = $stdin.getc
-  controller = ActionController.new(input, cat, nil, maze)
-  terminal.clear_cat
+  controller = ActionController.new(input, cat, treats, maze)
+  terminal.clear
   controller.process_input
 
-  terminal.render_cat
+  terminal.render
 rescue SignalException
   terminal.before_exit
   exit
--- views/terminal.rb
+++ views/terminal.rb
@@ -1,14 +1,18 @@
 class Terminal
-  def initialize(cat)
+  # def initialize allows us to call class.new
+  def initialize(cat, treats)
     @cat = cat
+    @treats = treats
   end
 
-  def render_cat
-    print "\033[#{@cat.y};#{@cat.x}H\033[0;33m#{@cat.icon} "
+  def render
+    render_cat
+    render_treats
   end
 
-  def clear_cat
-    print "\033[#{@cat.y};#{@cat.x}H\033[0;33m       "
+  def clear
+    clear_cat
+    clear_treats
   end
 
   # Initiaizes terminal and removes cursor and other settings
@@ -19,5 +23,35 @@ class Terminal
   # This undoes init updates
   def before_exit
     system 'tput reset; stty icanon; stty echo; clear'
+  end
+
+  private
+
+  def render_cat
+    print "\033[#{@cat.y};#{@cat.x}H\033[0;33m#{@cat.icon} "
+  end
+
+  def clear_cat
+    print "\033[#{@cat.y};#{@cat.x}H\033[0;33m       "
+  end
+
+  def render_treats
+    @treats.each do |treat|
+      render_treat(treat)
+    end
+  end
+
+  def render_treat(treat)
+    print "\033[#{treat.y};#{treat.x}H\033[0;33m#{treat.icon} "
+  end
+
+  def clear_treats
+    @treats.each do |treat|
+      clear_treat(treat)
+    end
+  end
+
+  def clear_treat(treat)
+    print "\033[#{treat.y};#{treat.x}H\033[0;33m "
   end
 end
